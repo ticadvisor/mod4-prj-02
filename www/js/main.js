e0bf36bf918bb5f88986c1f6aeee4cde60c159e2
ticadvisor <javier.herrero@ticadvisor.com>
2021-05-08 10:54:10 +0200

    Initial version v.0.0

---
+++ www/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Crea un reloj que se actualice en tiempo real:
+ *
+ *  - La hora debe ir en el <h1>
+ *  - La fecha debe ir en el <h2>
+ *
+ * En función de la hora del día la imagen de fondo debe cambiar.
+ * Para este punto puedes ayudarte de las clases:
+ *
+ *  - morning: a partir de las 7:00.
+ *
+ *  - afternoon: a partir de las 13:00.
+ *
+ *  - night: a partir de las 21:00.
+ *
+ */
